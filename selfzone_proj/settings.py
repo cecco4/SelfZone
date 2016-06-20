6456862d9f71c4b93c87a8effeb1fc96f8a3b384
Francesco Gatti <gattifrancesco@hotmail.it>
2016-06-20 23:36:17 +0200

    relative media path

--- selfzone_proj/settings.py
+++ selfzone_proj/settings.py
@@ -14,8 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = '/home/cecco/Documenti/django-projects/selfzone_proj/media/'
-MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 LOGIN_URL = '/login/'
 
 # Quick-start development settings - unsuitable for production
