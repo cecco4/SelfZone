90052cce39d159d913eb02df3735167753b8a092
cecco <cecco@server>
2016-07-04 06:34:55 +0200

    fix

--- init_database.sh
+++ init_database.sh
@@ -4,9 +4,9 @@ echo "  libmysqlclient-dev"
 echo "  virtualenv"
 read -p "press enter to continue"
 
-echo "  DROP DATABASE IF EXISTS db_name; CREATE DATABASE dbselfzone; \
-        CREATE USER IF NOT EXISTS 'cecco'@'localhost' IDENTIFIED BY 'cecco'; \
-        GRANT ALL PRIVILEGES ON dbselfzone.* TO 'cecco'@'localhost' WITH GRANT OPTION;" > init.sql
+echo -e "DROP DATABASE IF EXISTS dbselfzone; CREATE DATABASE dbselfzone;\
+\nCREATE USER 'cecco'@'localhost' IDENTIFIED BY 'cecco';\
+\nGRANT ALL PRIVILEGES ON dbselfzone.* TO 'cecco'@'localhost' WITH GRANT OPTION;" > init.sql
 
 echo "mysql access"
 mysql --user=root -p < init.sql
