37968259c5d54c07b4d627a9cc0bfadd7c445c8c
Francesco Gatti <gattifrancesco@hotmail.it>
2016-09-07 20:15:54 +0200

    charts

--- selfzone/views.py
+++ selfzone/views.py
@@ -216,6 +216,10 @@ class AreaChart(gchart.LineChart):
         return "graphos/gchart/area_chart.html"
 
 
+class PieChart(gchart.PieChart):
+    def get_template(self):
+        return "graphos/gchart/pie_chart.html"
+
 def stats(request):
     context = {}
 
@@ -234,6 +238,19 @@ def stats(request):
     anger       = Selfie.get_tagged("anger").count()
     happiness   = Selfie.get_tagged("happiness").count()
     surprise    = Selfie.get_tagged("surprise").count()
+
+    data = [("gender", "percentage"), ("male", male), ("female", female)]
+    chart = PieChart(SimpleDataSource(data=data), options={'title': "win vs loss"}, width="100%")
+    context['gender_chart'] = chart
+
+    data = [("age", "percentage"), ("baby", baby), ("young", young), ("old", old)]
+    chart = PieChart(SimpleDataSource(data=data), options={'title': "win vs loss"}, width="100%")
+    context['age_chart'] = chart
+
+    data = [("face expression", "percentage"), ("neutral", neutral), ("sad", sad),
+            ("anger", anger), ("happiness", happiness), ("surprise", surprise)]
+    chart = PieChart(SimpleDataSource(data=data), options={'title': "win vs loss"}, width="100%")
+    context['face_chart'] = chart
 
     context['allTimeBest']  = Selfie.objects.all().order_by("-score").all()[:3]
     context['allTimeWorst'] = reversed(Selfie.objects.all().order_by("score").all()[:3])
